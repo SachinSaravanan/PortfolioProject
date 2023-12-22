921c5921abc3c2d96e67f5243b17bad310c566b2
unknown <ssachinsachin113@gmail.com>
2023-12-22 10:42:01 +0530

    sachin pushing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PortfolioComponent ,ContactComponent ,FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
