98a80cbf9f9665ed9413aa4f7f479b222d48fb68
unknown <ssachinsachin113@gmail.com>
2024-01-23 13:05:21 +0530

    find

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PortfolioComponent ,ContactComponent ,FooterComponent ],
+  imports: [CommonModule, RouterOutlet, PortfolioComponent, ContactComponent, SkillsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
